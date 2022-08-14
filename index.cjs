be16fdbd8eccecf906e3ab300e0d451919cf164f
Timothy Quilling <teqed.arken@gmail.com>
2022-08-14 11:31:27 -0400

    prototyping 0

---
+++ index.cjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+let chalk = require('chalk');
+let inquirer = require('inquirer');
+let gradient = require('gradient-string');
+let chalkAnimation = require('chalk-animation');
+let figlet = require('figlet');
+let { createSpinner } = require('nanospinner');
+
+
+/*
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner'; */
+
+console.log(chalk.bgGreen('Hello world!'));
