a2875bf70b5b69903f256c3d85b86652d1c08784
Timothy Quilling <teqed.arken@gmail.com>
2022-08-20 00:36:31 -0400

    lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,74 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
+		es6: true,
 	},
-	extends: 'xo',
+	extends: [
+		"canonical",
+		"canonical/prettier",
+	],
 	overrides: [
 		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.mts',
-				'*.tsx',
-			],
+		  extends: [
+			"canonical/typescript",
+			"plugin:@typescript-eslint/recommended",
+			"plugin:@typescript-eslint/eslint-recommended",
+			'xo-typescript',
+			"xo",
+			"eslint:recommended",
+		  ],
+		  "files": "*.ts",
+		  "parserOptions": {
+			"project": "./tsconfig.json"
+		  }
 		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
+		{
+		  extends: [
+			"canonical/react",
+			"canonical/jsx-a11y",
+			"canonical/typescript",
+			"plugin:@typescript-eslint/recommended",
+			"plugin:@typescript-eslint/eslint-recommended",
+			'xo-typescript',
+			"xo",
+			"eslint:recommended",
+		  ],
+		  "files": "*.tsx",
+		  "parserOptions": {
+			"project": "./tsconfig.json"
+		  }
+		},
+		{
+		  extends: [
+			"canonical/jest"
+		  ],
+		  "files": "*.test.{ts,tsx}",
+		  "parserOptions": {
+			"project": "./tsconfig.json"
+		  }
+		},
+		{
+		  extends: [
+			"canonical/json"
+		  ],
+		  "files": "*.json"
+		},
+		{
+		  extends: [
+			"canonical/yaml"
+		  ],
+		  "files": "*.yaml"
+		},
+		{
+		  extends: [
+			"canonical/graphql"
+		  ],
+		  "files": "*.graphql"
+		}
+	  ],
+	  "root": true,
+	parser: "@typescript-eslint/parser",
 	rules: {
-	},
+	  },
+	  plugins: ["prettier", "@typescript-eslint"]
 };
