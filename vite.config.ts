ef33aaac4bc15c23e9ff66b0cb072db5cf4de5a6
Timothy Quilling <teqed.arken@gmail.com>
2022-08-27 02:21:50 -0400

    🔧 Update configuration

---
+++ vite.config.ts
@@ -0,0 +1,77 @@
+import { defineConfig } from 'vite';
+import { visualizer } from "rollup-plugin-visualizer";
+import progress from 'vite-plugin-progress'
+import { VitePluginNode } from 'vite-plugin-node';
+
+export default defineConfig({
+  // ...vite configures
+  server: {
+    // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+    port: 3000
+  },
+  plugins: [
+    visualizer(),
+    progress(),
+    ...VitePluginNode({
+      // Nodejs native Request adapter
+      // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
+      // you can also pass a function if you are using other frameworks, see Custom Adapter section
+      adapter: 'nest',
+
+      // tell the plugin where is your project entry
+      appPath: './src/index.ts',
+
+      // Optional, default: 'viteNodeApp'
+      // the name of named export of you app from the appPath file
+      exportName: 'viteNodeApp',
+
+      // Optional, default: 'esbuild'
+      // The TypeScript compiler you want to use
+      // by default this plugin is using vite default ts compiler which is esbuild
+      // 'swc' compiler is supported to use as well for frameworks
+      // like Nestjs (esbuild dont support 'emitDecoratorMetadata' yet)
+      // you need to INSTALL `@swc/core` as dev dependency if you want to use swc
+      tsCompiler: 'esbuild',
+
+      // Optional, default: {
+      // jsc: {
+      //   target: 'es2019',
+      //   parser: {
+      //     syntax: 'typescript',
+      //     decorators: true
+      //   },
+      //  transform: {
+      //     legacyDecorator: true,
+      //     decoratorMetadata: true
+      //   }
+      // }
+      // }
+      // swc configs, see [swc doc](https://swc.rs/docs/configuration/swcrc)
+      swcOptions: {}
+    })
+
+  ],
+  optimizeDeps: {
+    // Vite does not work well with optionnal dependencies,
+    // you can mark them as ignored for now
+    // eg: for nestjs, exlude these optional dependencies:
+    // exclude: [
+    //   '@nestjs/microservices',
+    //   '@nestjs/websockets',
+    //   'cache-manager',
+    //   'class-transformer',
+    //   'class-validator',
+    //   'fastify-swagger',
+    // ],
+    exclude: [
+      'aws-sdk',
+      'mock-aws-s3',
+      'nock',
+    ]
+  },
+  build: {
+    // vite build configs, for details see [vite doc](https://vitejs.dev/config/#build)
+    target: 'es2022',
+    polyfillModulePreload: true,
+  },
+});
