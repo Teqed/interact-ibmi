5f64c3d6e373c330e2c8b3126b3deb77a36f8f9d
Timothy Quilling <teqed.arken@gmail.com>
2022-08-30 15:22:19 -0400

    ✅ Add mocha test

---
+++ test/mocha/welcome.menus.test.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line node/no-unpublished-import
+import { expect } from 'chai';
+// eslint-disable-next-line import/extensions
+import { returnZero } from '../../build/util.js';
+
+// eslint-disable-next-line no-undef
+describe('#returnZero()', () => {
+	// eslint-disable-next-line no-undef
+	context('without arguments', () => {
+		// eslint-disable-next-line no-undef
+		it('should return 0', async () => {
+			const expectedValue = await returnZero();
+			expect(expectedValue).to.equal(0);
+		});
+	});
+});
