5d20aac33329c7eb876b4e73e2610172ea945a70
Timothy Quilling <teqed.arken@gmail.com>
2022-08-29 21:54:10 -0400

    🔨 Support dynamic imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,70 @@
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import progress from 'vite-plugin-progress';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import dynamicImport from 'vite-plugin-dynamic-import';
+import dts from 'vite-plugin-dts';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
 // import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
-	// ...vite configures
-	server: {
-		// vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
-		port: 3000,
+	build: {
+		minify: `esbuild`,
+		polyfillModulePreload: true,
+		rollupOptions: {
+			// external: [`aws-sdk`, `mock-aws-s3`, `nock`],
+			// external: [`#ansi-styles`, `#supports-color`],
+			input: `src/index.ts`,
+			output: {
+				chunkFileNames: `[name].chunk.js`,
+				dir: `dist`,
+				format: `es`,
+				// inlineDynamicImports: false,
+
+				// manualChunks(id) {
+				// 	if (id.includes(`node_modules`)) {
+				// 		return `vendor`;
+				// 	}
+				// },
+				// name: `app`,
+				plugins: [builtins, globals],
+
+				sourcemap: true,
+			},
+		},
+		// vite build configs, for details see [vite doc](https://vitejs.dev/config/#build)
+		target: `es2022`,
 	},
+
+	optimizeDeps: {
+		// Vite does not work well with optionnal dependencies,
+		// you can mark them as ignored for now
+		// eg: for nestjs, exlude these optional dependencies:
+		// exclude: [
+		//   '@nestjs/microservices',
+		//   '@nestjs/websockets',
+		//   'cache-manager',
+		//   'class-transformer',
+		//   'class-validator',
+		//   'fastify-swagger',
+		// ],
+		// exclude: [`aws-sdk`, `mock-aws-s3`, `nock`],
+	},
+
 	plugins: [
 		visualizer(),
 		progress(),
+		nodeResolve({
+			// browser: false,
+			exportConditions: [`node`],
+		}),
+		commonjs(),
+		dynamicImportVars,
+		dynamicImport,
+		dts(),
 		// ...VitePluginNode({
 		// Nodejs native Request adapter
 		// currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
@@ -51,24 +104,10 @@ export default defineConfig({
 		// swcOptions: {}
 		// })
 	],
-	optimizeDeps: {
-		// Vite does not work well with optionnal dependencies,
-		// you can mark them as ignored for now
-		// eg: for nestjs, exlude these optional dependencies:
-		// exclude: [
-		//   '@nestjs/microservices',
-		//   '@nestjs/websockets',
-		//   'cache-manager',
-		//   'class-transformer',
-		//   'class-validator',
-		//   'fastify-swagger',
-		// ],
-		exclude: [`aws-sdk`, `mock-aws-s3`, `nock`],
-	},
-	build: {
-		// vite build configs, for details see [vite doc](https://vitejs.dev/config/#build)
-		target: `es2022`,
-		polyfillModulePreload: true,
+	// ...vite configures
+	server: {
+		// vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+		port: 3_000,
 	},
 	test: {
 		exclude: [...`test/mocha/*.test.ts`, `**/test/mocha/**`, `**/node_modules/**`],
