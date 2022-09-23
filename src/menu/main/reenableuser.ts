9eb223fdce03b4befc26609f4c3c589de9cd31a4
Timothy Quilling <teqed.arken@gmail.com>
2022-09-23 16:00:10 -0400

    🥅 Handle user feedback

--- src/menu/main/reenableuser.ts
+++ src/menu/main/reenableuser.ts
@@ -1,8 +1,16 @@
+import chalk from 'chalk';
 import { cmdOdbc } from '../../odbc-util.js';
 import { CHGUSRPRF } from '../../qcmdexc-util.js';
 import pickUser from './pickuser.js';
 
 export default async function () {
 	const enableThisUser = await pickUser();
-	return await cmdOdbc(CHGUSRPRF({ STATUS: `*ENABLED`, USRPRF: enableThisUser }));
+	await cmdOdbc(CHGUSRPRF({ STATUS: `*ENABLED`, USRPRF: enableThisUser }))
+		.then(() => {
+			console.log(chalk.green(`User ${enableThisUser} has been re-enabled.`));
+		})
+		.catch(error => {
+			console.log(error);
+			console.log(chalk.yellow(`User ${enableThisUser} has not been re-enabled.`));
+		});
 }
