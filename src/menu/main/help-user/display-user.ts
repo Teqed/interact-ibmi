0769dce192016366182a46abc8364ebc9cb9a1d9
Timothy Quilling <teqed.arken@gmail.com>
2022-09-24 02:07:20 -0400

    🐛 Fix infinite spinner

--- src/menu/main/help-user/display-user.ts
+++ src/menu/main/help-user/display-user.ts
@@ -17,15 +17,13 @@ const fullUserInfo = async (user: string) => {
 const findUserPrompt = async () => {
 	// Create an array of strings containing menu choices made of the query results.
 	// Make sure that foundUsers is not undefined. If it is, sleep for 100 ms and try again.
+	const spinner = ora(`Finding users...`).start();
 	while (foundUsers === undefined) {
-		const spinner = ora(`Finding users...`).start();
 		// eslint-disable-next-line no-await-in-loop
 		await sleep(100);
-		if (foundUsers !== undefined) {
-			spinner.succeed(`Users found!`);
-			break;
-		}
 	}
+
+	spinner.succeed(`Users found!`);
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const choices = foundUsers.map((row: any) => {
