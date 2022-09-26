df8a733d7f0d9c966a6f0a4e4331e91debb709c4
Timothy Quilling <teqed.arken@gmail.com>
2022-09-26 13:15:54 -0400

    🎨 Add process title

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import login from './menu/login/login.js';
 import mainMenu from './menu/main/menu-main.js';
 // import { welcome } from './util.js';
 
+process.title = `interact-ibmi`;
+
 // eslint-disable-next-line import/no-mutable-exports
 let foundUserDiagnostics;
 
