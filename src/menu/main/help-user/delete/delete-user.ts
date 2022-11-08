dbbfcc72b5d031128a98504cca15cd733982b25a
Timothy Quilling <teqed.arken@gmail.com>
2022-11-08 13:14:02 -0500

    ✨ Use autocomplete prompt for delete-user

--- src/menu/main/help-user/delete/delete-user.ts
+++ src/menu/main/help-user/delete/delete-user.ts
@@ -1,12 +1,10 @@
+import chalk from 'chalk';
 import ora from 'ora';
 import { foundUsers } from '../../../../util/find-users.js';
 import deleteUser from '../../../../util/qcmdexc/dltusrprf.js';
-import qcmdexc from '../../../../util/qcmdexc/qcmdexc.js';
-import {
-	genericPressEnterPrompt,
-	generatedSelectMenu,
-	genericConfirmPrompt,
-} from '../../../generic/generic.js';
+import executeCommand from '../../../../util/qcmdexc/execute-command.js';
+import { genericPressEnterPrompt, genericConfirmPrompt } from '../../../generic/generic.js';
+import pickUser from '../pick-user.js';
 
 export default async function () {
 	const spinner = ora(`Checking...`).start();
@@ -14,18 +12,21 @@ export default async function () {
 	const users = foundUsers.map(user => user.AUTHORIZATION_NAME);
 	if (users.length > 0) {
 		spinner.succeed(`Users found!`);
-		const pickUserMenuChoice = await generatedSelectMenu({
-			choices: users,
-			message: `
-		Select a user from the list below to delete.
-		`,
-		});
+		console.clear();
+		console.log(
+			chalk.bgBlue(
+				// eslint-disable-next-line max-len
+				`\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Delete User\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0`,
+			),
+		);
+		const pickUserMenuChoice: string = await pickUser();
 		const confirm = await genericConfirmPrompt({
 			clearPromptOnDone: false,
 			message: `Are you sure you want to delete ${pickUserMenuChoice}?`,
 		});
 		if (confirm) {
-			await qcmdexc(deleteUser({ USRPRF: pickUserMenuChoice }));
+			// await qcmdexc(deleteUser({ USRPRF: pickUserMenuChoice }));
+			await executeCommand(deleteUser({ USRPRF: pickUserMenuChoice }));
 		}
 
 		return genericPressEnterPrompt();
